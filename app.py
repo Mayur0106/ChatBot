42bc40fc42207b6ab5a349da7868077e71358504
Mayur <mayurshinde0106@gmail.com>
2024-04-10 16:29:42 +0530

    GHJKL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 #from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 # def recycle_e_waste():
 #     # Logic to handle recycling e-waste
