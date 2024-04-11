4f6114e1a0db96aec9c698fe9a594f88e2f99e26
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2024-04-11 23:57:21 +0530

    Added functionality

--- app.py
+++ app.py
@@ -18,23 +18,28 @@ def donate_e_waste():
     # Logic to handle donating e-waste
     return "Consider donating your e-waste to organizations that refurbish electronics for reuse.", ["Charities Accepting E-Waste", "Reuse Centers"]
 
+def welcome_message():
+    return "Hello! How canI help you? A]Recycle B]Dispose C]Donate ", ["Recycle", "Dispose", "Donate"]
 @app.route('/dispose', methods=['POST'])
 def dispose():
     data = request.json
     message = data.get('message')
 
     # Process the message and provide appropriate response
-    if 'recycle' in message:
+    if any(word in message.lower() for word in ['hi', 'hello', 'hii']):
+        response, options = welcome_message()
+    elif any(word in message.lower() for word in [ 'recycling', 'recycle']):
         response, options = recycle_e_waste()
-    elif 'dispose' in message:
+    elif 'dispose' in message.lower():
         response, options = dispose_e_waste_safely()
-    elif 'donate' in message:
+    elif 'donate' in message.lower():
         response, options = donate_e_waste()
     else:
         response = "Sorry, I couldn't understand your request. Please try again."
         options = []
     
     return jsonify({'response': response, 'options': options})
+
 
 # if __name__ == "__main__":
 #     app.run(host="127.0.0.1", port=5000)
